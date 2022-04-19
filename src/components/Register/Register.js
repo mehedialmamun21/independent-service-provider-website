0654253e594a5a45c34b50469a56b4d7ee129460
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-19 07:14:40 +0600

    require auth component added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,18 +1,31 @@
 import React from "react";
 import {Link, useNavigate} from "react-router-dom";
 import "./Register.css";
+import {useCreateUserWithEmailAndPassword} from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Register = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
   const navigate = useNavigate();
 
   const navigateLogin = () => {
     navigate("/login");
   };
+
+  if (user) {
+    navigate("/");
+  }
+
   const handleRegister = (event) => {
     event.preventDefault();
     // console.log(event.target.email.value);
-    const name = event.target.password.value;
-    console.log(name);
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    createUserWithEmailAndPassword(email, password);
   };
   return (
     <div className="register-form">
