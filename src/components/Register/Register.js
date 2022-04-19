e0bf407cd2ec0cf9e5908b8cf09239b3f9fa1bac
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-19 06:35:40 +0600

    register part added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,52 @@
 import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+import "./Register.css";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const navigateLogin = () => {
+    navigate("/login");
+  };
+  const handleRegister = (event) => {
+    event.preventDefault();
+    // console.log(event.target.email.value);
+    const name = event.target.password.value;
+    console.log(name);
+  };
   return (
-    <div>
-      <h2>Please Register</h2>
-      <form action=""></form>
+    <div className="register-form">
+      <h2 style={{textAlign: "center"}}>Please Register</h2>
+      <form onSubmit={handleRegister}>
+        <input type="text" name="name" id="" placeholder="your name" />
+
+        <input
+          type="email"
+          name="email"
+          id=""
+          placeholder="email address"
+          required
+        />
+
+        <input
+          type="password"
+          name="password"
+          id=""
+          placeholder="password"
+          required
+        />
+        <input type="submit" value="Register" />
+      </form>
+      <p>
+        Already have an account?
+        <Link
+          to={"/login"}
+          className="text-danger text-decoration-none"
+          onClick={navigateLogin}
+        >
+          Please Login
+        </Link>
+      </p>
     </div>
   );
 };
