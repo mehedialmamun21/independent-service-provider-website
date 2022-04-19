d7ff76a1a7604083b646ba24808455bdcfe891ee
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-19 23:55:25 +0600

    some more functionality added

--- src/components/ServiceDetail/ServiceDetail.js
+++ src/components/ServiceDetail/ServiceDetail.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./ServiceDetail.css";
 
 const ServiceDetail = () => {
   return (
-    <div>
+    <div className="service-section">
       <h2>Welcome to service detail section</h2>
       <div className="text-center">
         <Link to="/checkout">
